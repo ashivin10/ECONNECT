1caefcc4bdc314a60c3ec8a711a189a7109609ad
NANDINE <nandine0804@>
2023-04-02 23:38:05 +0530

    completed login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import { useState } from "react";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SignUp from "./components/SignUp";
+
 import Login from "./components/Login";
 
 function App() {
